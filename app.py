cbbe3ce334a832f1da447ef6bc6238d61d2072ac
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-04-05 21:34:34 +0530

    post method added

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app=Flask(__name__)
 @app.route("/")
 def home():
 	return render_template('index.html')
-@app.route("/signup")
+@app.route("/signup",methods=["POST"])
 def signup():
-	print(request.form)
+	print(request.form["name"])
 	return jsonify(response="success")
 if __name__=="__main__":
 	app.run(debug=True)
