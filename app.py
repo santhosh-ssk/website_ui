f8d5289797927de7f54a8d7afdddd565e8d57a30
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-04-05 20:15:21 +0530

    mongodb

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
+from mongoengine import connect
+connect(
+    db='website_ui',
+    username='admin',
+    password='admin123',
+    host='mongodb://admin:admin123@ds131384.mlab.com:31384/website_ui'
+)
+
 app=Flask(__name__)
 
 @app.route("/")
 def home():
 	return render_template('index.html')
-
+@app.route("/signup")
+def signup():
+	print(request.form)
+	return jsonify(response="success")
 if __name__=="__main__":
 	app.run(debug=True)
