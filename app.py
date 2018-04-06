4ef1a34ba741b587027ed5fd6ef4fb6e0d29e96d
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-04-06 19:27:23 +0530

    restorents api added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,jsonify
+import requests
+import json
+maps_api_key="AIzaSyDLd9n_vxzY4vt-wDC17Sx1TlVpANYCsY4"
 app=Flask(__name__)
 
 @app.route("/")
@@ -9,5 +12,31 @@ def home():
 def map():
 	return render_template('first_map.html')
 
+@app.route('/api/vi/list_shops/<lat>/<log>')
+def list_shops(lat,log):
+	print(lat,log)
+	resp=requests.request("GET","https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+log+"&radius=1000&type=restaurant&key="+maps_api_key)
+	locations=json.loads(resp.content)['results']
+	respons=list()
+	
+	for location in locations:
+		result={
+			'lat':location['geometry']['location']['lat'],
+			'lng':location['geometry']['location']['lng'],
+			'name':location['name'],	
+			'place_id':location['place_id'],
+			'address':location['vicinity'],
+			}
+		if 'photos' in location.keys():
+			result['photos']=[x['photo_reference'] for x in location['photos']]
+		else:
+			result['photos']=[]
+		if 'rating' in location.keys():
+			result['rating']=location['rating']
+		else:
+			result['rating']='Not Available'
+		respons.append(result)
+	return jsonify(respons)	
+	
 if __name__=="__main__":
 	app.run(debug=True)
