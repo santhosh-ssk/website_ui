0f9591fa5f37deb4bf0ff13cb57e93fd2c38056c
Natarajan <natarajm1998@gmail.com>
2018-04-06 09:45:09 +0530

    map_cluster

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app=Flask(__name__)
 def home():
 	return render_template('index.html')
 
+@app.route("/first_map")
+def map():
+	return render_template('first_map.html')
+
 if __name__=="__main__":
 	app.run(debug=True)
